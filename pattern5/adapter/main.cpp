01a4ae5a1564c65e59fc1a59ea674939652b3cd0
konisandro <54888293+konisandro@users.noreply.github.com>
2019-12-16 20:05:28 +0700

    Add files via upload

---
+++ pattern5/adapter/main.cpp
@@ -0,0 +1,29 @@
+#include <QCoreApplication>
+#include "mallardduck.h"
+#include "wildturkey.h"
+#include "turkeyadapter.h"
+
+void testDuck(Duck *duck){
+    duck->quack();
+    duck->fly();
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    MallardDuck *duck = new MallardDuck();
+    WildTurkey *turkey = new WildTurkey();
+    Duck *turkeyAdapter = new TurkeyAdapter(turkey);
+
+    cout<< "The Turkey says..." <<endl;
+    turkey->gobble();
+    turkey->fly();
+
+    cout<<"\nThe Duck says..." <<endl;
+    testDuck(duck);
+
+    cout<<"\nThe TurkeyAdapter says..." <<endl;
+    testDuck(turkeyAdapter);
+    return a.exec();
+}
---
+++ pattern5/adapter/wildturkey.h
@@ -0,0 +1,14 @@
+#ifndef WILDTURKEY_H
+#define WILDTURKEY_H
+#include "turkey.h"
+
+class WildTurkey : public Turkey
+{
+public:
+    WildTurkey();
+    ~WildTurkey();
+    void gobble();
+    void fly();
+};
+
+#endif // WILDTURKEY_H
